5ec33e7b93cfc36ad9199f9a9450f99e47863ff0
Tyuiu.AvaevaPD@outlook.com <Tyuiu.AvaevaPD@outlook.com>
2023-10-04 14:33:15 +0500

    Task2 complete

---
+++ Tyuiu.AvaevaPD.Sprint1.Task2.V0/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Tyuiu.AvaevaPD.Sprint1.Task2.V0.Lib;
+
+//ЗАДАНИЕ
+//Написать программу, которая запрашивает у пользователя исходные данные,
+//выполняет указанные расчеты и печатает результат на экране.
+//Расчеты: Известно число. Найти его квадрат.
+//Число (целое число)
+//Квадрат числа (целое число)
+
+namespace Tyuiu.AvaevaPD.Sprint1.Task2.V0
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataService ds = new DataService();
+
+            Console.Title = "Спринт #1 | Выполнила: Авaева П. Д. | ИСПБ-23-1";
+            // Длина строки 75 символов
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Спринт #1                                                               *");
+            Console.WriteLine("* Тема: Организация ввода или вывода в консольных приложениях             *");
+            Console.WriteLine("* Задание #2                                                              *");
+            Console.WriteLine("* Вариант #0                                                              *");
+            Console.WriteLine("* Выполнила: Аваева Полина Дмитриевна | ИСПБ-23-1                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* УСЛОВИЕ:                                                                *");
+            Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+            Console.WriteLine("* выполняет указанные расчеты и печатает результат на экране.             *");
+            Console.WriteLine("*                                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* Квадрат числа X                                                         *");
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            int x;
+            Console.WriteLine("Введите значение X:");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("***************************************************************************");
+            Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+            Console.WriteLine("***************************************************************************");
+
+            Console.WriteLine("Квадрат числа X = " + ds.Sqr(x));
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ example/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+
+            int i = 5;
+
+            double f = 7.5;
+
+            double dbl = 10.01;
+
+            Console.WriteLine("i=" + i + "\\f=" + f + "\\dbl=" + dbl);
+        }
+    }
+}
